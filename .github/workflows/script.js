408c12a8d6943b3713005e8a37edc2bd666c07d4
vickyvicky0214-rgb <vickyvicky87780@gmail.com>
2026-03-11 21:12:55 +0530

    Add files via upload

---
+++ .github/workflows/script.js
@@ -0,0 +1,17 @@
+var countDownDate = new Date("June 14, 2026 00:00:00").getTime();
+
+setInterval(function(){
+
+var now = new Date().getTime();
+
+var distance = countDownDate - now;
+
+var days = Math.floor(distance/(1000*60*60*24));
+var hours = Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+var minutes = Math.floor((distance%(1000*60*60))/(1000*60));
+var seconds = Math.floor((distance%(1000*60))/1000);
+
+document.getElementById("countdown").innerHTML =
+days+"d "+hours+"h "+minutes+"m "+seconds+"s";
+
+},1000);
